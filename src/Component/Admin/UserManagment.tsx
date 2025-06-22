106a701b59a6a9b1ee186f0b15bb9572870fee4e
manish <manishkeer530@gmail.com>
2025-06-22 15:27:28 +0530

    navbar added in admin

--- src/Component/Admin/UserManagment.tsx
+++ src/Component/Admin/UserManagment.tsx
@@ -272,6 +272,7 @@ import {
 import { toast } from "react-hot-toast";
 import { UsersState } from "../../features/User/UserSlice";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../Navbar";
 
 const UserManagement = () => {
     const navigate = useNavigate();
@@ -473,6 +474,9 @@ const UserManagement = () => {
     }
 
     return (
+        <>
+            <Navbar/>
+    
         <div className="min-h-screen bg-gray-950 p-4 sm:p-6 md:p-8">
             <div className="max-w-7xl mx-auto space-y-6">
                 <motion.h1
@@ -895,6 +899,7 @@ const UserManagement = () => {
                 </AnimatePresence>
             </div>
         </div>
+            </>
     );
 };
 
